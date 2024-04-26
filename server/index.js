48c822e55cf6c61577949926c5773de81fbfae26
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-26 16:20:19 +0300

    update the index page

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import { config } from "dotenv"; // Import config function from 'dotenv'
 import { MONGODB_URL, PORT } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModels.js";
 import bookRoutes from './routes/bookRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -13,6 +13,12 @@ app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN stack Tutorial!");
 });
+
+app.use(cors({
+  origin:true,
+  methods:['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders:['Content-type'],
+}));
 
 app.use("/books", bookRoutes);
 
