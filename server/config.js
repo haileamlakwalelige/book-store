5d0dab63fa2ff254780b4990ee9bfe6879fccae7
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-26 16:20:10 +0300

    change the database

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
 export const PORT = 3003;
-export const MONGODB_URL="mongodb+srv://Haileopia:0939100302@book-store.uumy9pz.mongodb.net/?retryWrites=true&w=majority&appName=book-store"
+export const MONGODB_URL="mongodb://localhost:27017/bookstore"
