9e80eb0e441a9ae4e22571685ca3224addf7ddbb
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-26 16:18:33 +0300

    update and adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+import DeleteBooks from "./pages/DeleteBooks";
+import CreateBooks from "./pages/CreateBooks";
+import EditBook from "./pages/EditBook";
+import Detail from "./pages/Detail";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/books/show/:id" element={<ShowBook />} />
+    <Route path="/books/detail/:id" element={<Detail />} />
+    <Route path="/books/delete/:id" element={<DeleteBooks />} />
+    <Route path="/books/create" element={<CreateBooks />} />
+    <Route path="/books/edit/:id" element={<EditBook />} />
+     </Routes>
+   </div>
   )
 }
